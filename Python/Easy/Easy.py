0f866996e45c279be9cddb95c9847cc3f50776ee
dellanie <a.lanre11@yahoo.com>
2022-09-22 06:02:50 -0400

    solved

--- Python/Easy/Easy.py
+++ Python/Easy/Easy.py
@@ -1,2 +1,9 @@
-#starting with this first thing in the morning
-#last day pf procastinnating
+#length Of Last Word
+#spliting the sentence into words.Then finding the length of the last array using -1.
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        self.length = s.strip().split()
+        track_list=[]
+        track_list = self.length
+        last_word = track_list[-1]
+        return len(last_word)
